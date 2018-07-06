3f2b953719ea2c8d3fcc0cfe8fbb3ffc76da9322
xfap <1016616369@qq.com>
2018-07-06 21:24:52 +0800

    xfap:建表语句的修正，和测试sql的编写

---
+++ server/testdata_insert.sql
@@ -0,0 +1,25 @@
+-- CREATE TABLE successor(
+	-- USER_ID VARCHAR2(24) PRIMARY KEY,
+  -- username  VARCHAR2(64) NOT NULL,
+  -- area_name VARCHAR2(32),
+  -- dorm_name VARCHAR2(32)
+	-- );
+insert into successor values ('test_usrid_1', 'user1', '华山区','14栋612');
+insert into successor values ('test_usrid_2', 'user2', '泰山区','8栋410');
+insert into successor values ('test_usrid_3', 'user3', '黑山区','2栋312');
+insert into successor values ('test_usrid_4', 'user4', '启林区','1栋212');
+
+ -- CREATE TABLE suc_stuff(
+	-- suc_id NUMBER(24) PRIMARY KEY ,
+  -- suc_title VARCHAR2(64) NOT NULL,
+  -- suc_intro VARCHAR2(1024),
+  -- suc_pic_url VARCHAR2(32),
+  -- suc_class VARCHAR2(32),
+  -- suc_publish_time VARCHAR2(32)
+  -- );
+
+insert into suc_stuff values ('test_usrid_1', 'user1', '华山区','14栋612');
+insert into suc_stuff values ('test_usrid_2', 'user2', '泰山区','8栋410');
+insert into suc_stuff values ('test_usrid_3', 'user3', '黑山区','2栋312');
+insert into suc_stuff values ('test_usrid_4', 'user4', '启林区','1栋212');
+  
