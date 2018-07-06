18788ec71fecb24862504824a0ad37decbfdb10d
xfap <1016616369@qq.com>
2018-07-06 20:56:59 +0800

    sql文件修改

--- server/sql_create.sql
+++ server/sql_create.sql
@@ -1,21 +1,30 @@
+drop table successor;
+drop table suc_stuff;
+drop table suc_request;
+drop table suc_process;
 CREATE TABLE successor(
-	USER_ID NUMBER(24) PRIMARY KEY,
+	USER_ID VARCHAR2(24) PRIMARY KEY,
   username  VARCHAR2(64) NOT NULL,
   area_name VARCHAR2(32),
   dorm_name VARCHAR2(32)
 	);
  CREATE TABLE suc_stuff(
-	suc_id NUMBER(24) PRIMARY KEY,
+	suc_id VARCHAR2(24) PRIMARY KEY,
   suc_title VARCHAR2(64) NOT NULL,
   suc_intro VARCHAR2(1024),
   suc_pic_url VARCHAR2(32),
   suc_class VARCHAR2(32),
   suc_publish_time VARCHAR2(32)
   );
- CREATE TABLE suc_process(
-     suc_id NUMBER(24) PRIMARY KEY,
+ 
+ CREATE TABLE suc_request(
+     suc_id VARCHAR2(24) PRIMARY KEY,
+     user_id VARCHAR2(24),
      need_flag NUMBER(1),
-     comfirm_give_flag NUMBER(1),
+     comfirm_give_flag NUMBER(1)
+);
+ CREATE TABLE suc_process(
+     suc_id VARCHAR2(24) PRIMARY KEY,
      owner_spare_time_start VARCHAR2(24),
      owner_spare_time_end VARCHAR2(24),
      confirm_recv NUMBER(1),
