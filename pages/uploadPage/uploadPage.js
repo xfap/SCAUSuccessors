8c24cdf04a4aa2def72471b1e18021d15ab56a83
Lujunjiatry <1362153256@qq.com>
2018-07-07 13:34:13 +0800

    lu add

--- pages/uploadPage/uploadPage.js
+++ pages/uploadPage/uploadPage.js
@@ -1,11 +1,11 @@
 // pages/uploadPage/uploadPage.js
 Page({
-
+ 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    productInfo: {}
   },
 
   /**
@@ -62,5 +62,38 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  bindChooiceProduct:function(){
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths;
+        wx.uploadFile({
+          url: 'https://127.0.0.1.com',      //此处换上你的接口地址
+          filePath: tempFilePaths[0],
+          name: 'img',
+          header: {
+            "Content-Type": "multipart/form-data",
+            'accept': 'application/json',
+            // 'Authorization': 'Bearer ..'    //若有token，此处换上你的token，没有的话省略
+          },
+          formData: {
+            'user': 'test'  //其他额外的formdata，可不写
+          },
+          success: function (res) {
+            var data = res.data;
+            console.log('data');
+          },
+          fail: function (res) {
+            console.log('fail');
+
+          },
+        })
+      }
+    })
+
   }
 })
