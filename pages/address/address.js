6ea18d7faba6fa0ed579b8895a112aa2b263d2c7
wolkenmeer911 <www.490757364@qq.com>
2018-07-09 10:23:31 +0800

    Eli新增上传用例

--- pages/address/address.js
+++ pages/address/address.js
@@ -8,7 +8,8 @@ Page({
     address: '',
     room: '',
     input_address: '',
-    input_room: ''
+    input_room: '',
+    url: getApp().globalData.serverhome
   },
 
   modify: function() {
@@ -30,25 +31,38 @@ Page({
       address: this.data.input_address,
       room: this.data.input_room
     })
-    console.log('address:' + this.data.address)
-    console.log('room:' + this.data.room)
+    //console.log('address:' + this.data.address)
+    //console.log('room:' + this.data.room)
+    //console.log(getApp().globalData.serverhome)
     wx.request({
-      url: '192.168.253.1',
-      area_name: this.data.address,
-      dorm_name: this.data.room
+      url: this.data.url,
+      data: {
+        stype: 'address',
+        user_id: '10001',
+        area_name: this.data.address,
+        dorm_name: this.data.room
+      },
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function (res) {
+        console.log(res)
+        // fail
+      }
     })
+    console.log('testing')
     wx.showToast({
       title: '成功',
-      icon : 'success',
+      icon: 'success',
       duration: 2000,
       mask: true
     })
-    setTimeout(function(){
+    setTimeout(function() {
       wx.navigateBack({
         changed: true
       })
-    },2000)
-    
+    }, 2000)
+
   },
 
   inAddress: function(e) {
