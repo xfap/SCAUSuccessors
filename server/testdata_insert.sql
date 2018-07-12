c58849bfcf09878a75dd07841133b565956d130d
wolkenmeer911 <www.490757364@qq.com>
2018-07-12 15:26:08 +0800

    Eli修改测试数据

--- server/testdata_insert.sql
+++ server/testdata_insert.sql
@@ -13,79 +13,38 @@ insert into successor values ('test_usrid_2', 'user2', '泰山区','8栋410');
 insert into successor values ('test_usrid_3', 'user3', '黑山区','2栋312');
 insert into successor values ('test_usrid_4', 'user4', '启林区','1栋212');
 
---new LU
+--new 
 insert into successor values ('test_usrid_5', 'user5', '华山区','14栋609');
 insert into successor values ('test_usrid_6', 'user6', '嵩山区','6栋606');
 insert into successor values ('test_usrid_7', 'user7', '华山区','1栋111');
 insert into successor values ('test_usrid_8', 'user8', '泰山区','2栋201');
-insert into successor values ('test_usrid_9', 'user9', '黑山区','3栋301');
-insert into successor values ('test_usrid_10', 'user10', '启林区','4栋401');
-insert into successor values ('test_usrid_11', 'user11', '华山区','5栋501');
-insert into successor values ('test_usrid_12', 'user12', '嵩山区','6栋601');
-insert into successor values ('test_usrid_13', 'user13', '华山区','7栋701');
 --可以尝试做出 继承物的生命周期
 --时序图、状态机图等等（软件工程应用）
 --------------------------------successor---------------------------------------------
 --处于发布状态的物品 图片url 使用物品id 和 时间 联合命名
 --------------------------------suc_stuff---------------------------------------------
 
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_1','山灵M0','山灵便携播放器','./images/1-201807051010.png','数码','20180705101023');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_1','艾巴索DX150','艾巴索播放器','./images/2-201807052025.png','数码','20180705202534');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_2','四级词汇书','新东方四级单词书','./images/3-201807062025.png','书籍','20180706202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_2','六级词汇书','新东方六级单词书','./images/4-201807072025.png','书籍','20180707202545');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_2','无印笔盒','','./images/5-201807082025.png','文具','20180708202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_2','三菱笔芯','','./images/6-201807092025.png','文具','20180709202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_3','蓝牙鼠标','','./images/7-201807102025.png','数码','20180710202523');
+1  insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_1','山灵M0','山灵便携播放器','./images/1-201807051010.png','数码','20180705101023');
+2  insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_1','艾巴索DX150','艾巴索播放器','./images/2-201807052025.png','数码','20180705202534');
+3  insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_2','四级词汇书','新东方四级单词书','./images/3-201807062025.png','书籍','20180706202512');
+4  insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_2','六级词汇书','新东方六级单词书','./images/4-201807072025.png','书籍','20180707202545');
+5  insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_2','无印笔盒','','./images/5-201807082025.png','文具','20180708202512');
+6  insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_2','三菱笔芯','','./images/6-201807092025.png','文具','20180709202512');
+7  insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_3','蓝牙鼠标','','./images/7-201807102025.png','数码','20180710202523');
 --new
 
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_4','QQ爱','','./images/7-201807102025.png','玩具','20180714202523');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_4','爱卿','','./images/7-201807102025.png','玩具','20180713112314');
+8  insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_4','QQ爱','','./images/7-201807102025.png','玩具','20180714202523');
+9  insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_4','爱卿','','./images/7-201807102025.png','玩具','20180713112314');
 
---new LU
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_5','四级词汇书','新东方四级单词书','./images/3-201807062025.png','书籍','20180706202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_5','六级词汇书','新东方六级单词书','./images/4-201807072025.png','书籍','20180707202545');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_5','无印笔盒','','./images/5-201807082025.png','文具','20180708202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_5','蓝牙鼠标','','./images/7-201807102025.png','数码','20180711150723');
+--new 
+10 insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_3','名创优品收纳盒','三格收纳盒','./images/3-201807062025.png','生活用品','20180710202512');
+11 insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_3','名创优品收纳盒','五格收纳盒','./images/4-201807072025.png','生活用品','20180710203545');
+12 insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_4','鸭舌帽','NY鸭舌帽','./images/5-201807082025.png','服饰','20180711202512');
+13 insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_4','鸭舌帽','MLB鸭舌帽','./images/7-201807102025.png','服饰','20180711150723');
+14 insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_5','鼠标垫','','./images/7-201807102025.png','数码','20180711160723');
+15 insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_5','iPhone6S手机壳','全包软壳','./images/7-201807102025.png','数码','20180711152223');
+16 insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_5','iPhone6S手机壳','浮雕磨砂壳','./images/7-201807102025.png','数码','20180711163354');
 
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_6','四级词汇书','新东方四级单词书','./images/3-201807062025.png','书籍','20180706202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_6','六级词汇书','新东方六级单词书','./images/4-201807072025.png','书籍','20180707202545');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_6','无印笔盒','','./images/5-201807082025.png','文具','20180708202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_6','蓝牙鼠标','','./images/7-201807102025.png','数码','20180711150723');
-
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_7','四级词汇书','新东方四级单词书','./images/3-201807062025.png','书籍','20180706202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_7','六级词汇书','新东方六级单词书','./images/4-201807072025.png','书籍','20180707202545');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_7','无印笔盒','','./images/5-201807082025.png','文具','20180708202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_7','蓝牙鼠标','','./images/7-201807102025.png','数码','20180711150723');
-
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_8','四级词汇书','新东方四级单词书','./images/3-201807062025.png','书籍','20180706202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_8','六级词汇书','新东方六级单词书','./images/4-201807072025.png','书籍','20180707202545');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_8','无印笔盒','','./images/5-201807082025.png','文具','20180708202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_8','蓝牙鼠标','','./images/7-201807102025.png','数码','20180711150723');
-
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_9','四级词汇书','新东方四级单词书','./images/3-201807062025.png','书籍','20180706202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_9','六级词汇书','新东方六级单词书','./images/4-201807072025.png','书籍','20180707202545');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_9','无印笔盒','','./images/5-201807082025.png','文具','20180708202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_9','蓝牙鼠标','','./images/7-201807102025.png','数码','20180711150723');
-
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_10','四级词汇书','新东方四级单词书','./images/3-201807062025.png','书籍','20180706202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_10','六级词汇书','新东方六级单词书','./images/4-201807072025.png','书籍','20180707202545');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_10','无印笔盒','','./images/5-201807082025.png','文具','20180708202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_10','蓝牙鼠标','','./images/7-201807102025.png','数码','20180711150723');
-
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_11','四级词汇书','新东方四级单词书','./images/3-201807062025.png','书籍','20180706202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_11','六级词汇书','新东方六级单词书','./images/4-201807072025.png','书籍','20180707202545');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_11','无印笔盒','','./images/5-201807082025.png','文具','20180708202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_11','蓝牙鼠标','','./images/7-201807102025.png','数码','20180711150723');
-
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_12','四级词汇书','新东方四级单词书','./images/3-201807062025.png','书籍','20180706202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_12','六级词汇书','新东方六级单词书','./images/4-201807072025.png','书籍','20180707202545');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_12','无印笔盒','','./images/5-201807082025.png','文具','20180708202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_12','蓝牙鼠标','','./images/7-201807102025.png','数码','20180711150723');
-
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_13','四级词汇书','新东方四级单词书','./images/3-201807062025.png','书籍','20180706202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_13','六级词汇书','新东方六级单词书','./images/4-201807072025.png','书籍','20180707202545');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_13','无印笔盒','','./images/5-201807082025.png','文具','20180708202512');
-insert into suc_stuff(USER_ID,suc_title,suc_intro,suc_pic_url,suc_class,suc_publish_time) values ('test_usrid_13','蓝牙鼠标','','./images/7-201807102025.png','数码','20180711150723');
 --------------------------------suc_stuff---------------------------------------------
  -- CREATE TABLE suc_request(
      -- suc_id NUMBER(32) PRIMARY KEY,
@@ -106,72 +65,37 @@ insert into suc_request values('4','test_usrid_4',1,'20180708202523');
 insert into suc_request values('5','test_usrid_4',1,'20180709202512');
 insert into suc_request values('6','test_usrid_4',1,'20180709202532');
 
---new LU
-insert into suc_request values('7','test_usrid_5',1,'20180709202512');
-insert into suc_request values('8','test_usrid_5',1,'20180709202545');
-insert into suc_request values('9','test_usrid_5',1,'20180709202512');
-insert into suc_request values('10','test_usrid_5',1,'20180711150723');
+--new 
+insert into suc_request values('7','test_usrid_5',1,'20180710212512');
+insert into suc_request values('8','test_usrid_5',1,'20180714203845');
+insert into suc_request values('9','test_usrid_5',1,'20180713202512');
 
-insert into suc_request values('11','test_usrid_6',1,'20180709202512');
-insert into suc_request values('12','test_usrid_6',1,'20180709202545');
-insert into suc_request values('13','test_usrid_6',1,'20180709202512');
-insert into suc_request values('14','test_usrid_6',1,'20180711150723');
-insert into suc_request values('42','test_usrid_13',1,'20180711150723');
+
+insert into suc_request values('10','test_usrid_5',0,'20180711150723');
+insert into suc_request values('11','test_usrid_6',0,'20180710222512');
+insert into suc_request values('12','test_usrid_6',-1,'20180711204545');
+insert into suc_request values('13','test_usrid_6',-1,'20180712202512');
+insert into suc_request values('14','test_usrid_7',1,'20180712230723');
+insert into suc_request values('15','test_usrid_7',1,'20180712150723');
+insert into suc_request values('16','test_usrid_7',1,'20180712150723');
 --------------------------------suc_request---------------------------------------------
 
 --处于继承过程的物品
 --------------------------------suc_process---------------------------------------------
 
 insert into suc_process values('3','20180710180123','20180710220123',0,0,null);
-insert into suc_process values('4','20180710131123','20180710181123',1,0,'20180709202512');
-insert into suc_process values('5','20180710141123','20180710201123',0,1,'20180711101312');
-insert into suc_process values('6','20180710081123','20180710131123',1,1,'20180712143212');
+insert into suc_process values('4','20180710131123','20180710181123',0,0,null);
+insert into suc_process values('5','20180710141123','20180710201123',0,1,null);
+insert into suc_process values('6','20180710081123','20180710131123',0,1,null);
 
---new LU
-insert into suc_process values('7','20180710081123','20180710131123',0,0,'20180712142512');
-insert into suc_process values('8','20180710131123','20180710181123',1,0,'20180709202512');
-insert into suc_process values('9','20180710141123','20180710201123',0,1,'20180711101312');
-insert into suc_process values('10','20180710081123','20180710131123',1,1,'20180712143212');
+--new 
+insert into suc_process values('7','20180710081123','20180710131123',0,1,null);
+insert into suc_process values('8','20180710131123','20180710181123',1,0,null);
+insert into suc_process values('9','20180710141123','20180710201123',1,0,null);
+insert into suc_process values('14','20180710081123','20180710131123',1,0,null);
+insert into suc_process values('15','20180710081123','20180710131123',1,1,'20180712143212');
+insert into suc_process values('16','20180710081123','20180710131123',1,1,'20180712143212');
 
-insert into suc_process values('11','20180710081123','20180710131123',0,0,'20180712142512');
-insert into suc_process values('12','20180710131123','20180710181123',1,0,'20180709202512');
-insert into suc_process values('13','20180710141123','20180710201123',0,1,'20180711101312');
-insert into suc_process values('14','20180710081123','20180710131123',1,1,'20180712143212');
-
-insert into suc_process values('15','20180710081123','20180710131123',0,0,'20180712142512');
-insert into suc_process values('16','20180710131123','20180710181123',1,0,'20180709202512');
-insert into suc_process values('17','20180710141123','20180710201123',0,1,'20180711101312');
-insert into suc_process values('18','20180710081123','20180710131123',1,1,'20180712143212');
-
-insert into suc_process values('19','20180710081123','20180710131123',0,0,'20180712142512');
-insert into suc_process values('20','20180710131123','20180710181123',1,0,'20180709202512');
-insert into suc_process values('21','20180710141123','20180710201123',0,1,'20180711101312');
-insert into suc_process values('22','20180710081123','20180710131123',1,1,'20180712143212');
-
-insert into suc_process values('23','20180710081123','20180710131123',0,0,'20180712142512');
-insert into suc_process values('24','20180710131123','20180710181123',1,0,'20180709202512');
-insert into suc_process values('25','20180710141123','20180710201123',0,1,'20180711101312');
-insert into suc_process values('26','20180710081123','20180710131123',1,1,'20180712143212');
-
-insert into suc_process values('27','20180710081123','20180710131123',0,0,'20180712142512');
-insert into suc_process values('28','20180710131123','20180710181123',1,0,'20180709202512');
-insert into suc_process values('29','20180710141123','20180710201123',0,1,'20180711101312');
-insert into suc_process values('30','20180710081123','20180710131123',1,1,'20180712143212');
-
-insert into suc_process values('31','20180710081123','20180710131123',0,0,'20180712142512');
-insert into suc_process values('32','20180710131123','20180710181123',1,0,'20180709202512');
-insert into suc_process values('33','20180710141123','20180710201123',0,1,'20180711101312');
-insert into suc_process values('34','20180710081123','20180710131123',1,1,'20180712143212');
-
-insert into suc_process values('35','20180710081123','20180710131123',0,0,'20180712142512');
-insert into suc_process values('36','20180710131123','20180710181123',1,0,'20180709202512');
-insert into suc_process values('37','20180710141123','20180710201123',0,1,'20180711101312');
-insert into suc_process values('38','20180710081123','20180710131123',1,1,'20180712143212');
-
-insert into suc_process values('39','20180710081123','20180710131123',0,0,'20180712142512');
-insert into suc_process values('40','20180710131123','20180710181123',1,0,'20180709202512');
-insert into suc_process values('41','20180710141123','20180710201123',0,1,'20180711101312');
-insert into suc_process values('42','20180710081123','20180710131123',1,1,'20180712143212');
 --------------------------------suc_process---------------------------------------------
 
 
